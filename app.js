7d939a3323aa5f3d79c6318ae0ca490a13bb2017
nave cohen <nave.cohen@e.braude.ac.il>
2023-05-27 17:48:59 +0300

    change jwt from app.get to app.use

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ app.set('view engine', 'ejs')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(sessions)
 
 app.use('/public', express.static(__dirname + '/public'))
+app.use(sessions)
+app.use(jwt)
+
 app.use('/users', users)
 
-app.get('/', jwt, function (req, res) {
-    res.render('ejs/index.ejs')
-})
+
 
 app.listen(3000, () => console.log('server started'))
--- middlewares/login.middleware.js
+++ middlewares/login.middleware.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 const jwt = router.use((req, res, next) => {
-    if (!req.session.user) {
-        res.redirect('users/signin');
+    if (!req.session.user && (!req.url.includes('users/signin') && !req.url.includes('users/signup'))) {
+        res.redirect('/users/signin');
     } else {
         next();
     }
