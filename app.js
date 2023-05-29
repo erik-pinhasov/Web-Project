ddb3cfb7130f2c3fea7c74646078d70da0a5954b
nave cohen <nave.cohen@e.braude.ac.il>
2023-05-29 12:49:46 +0300

    design login page

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 /* eslint-disable no-undef */
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const users = require('./routes/users');
-const sessions = require('./services/sessions.service');
-const jwt = require('./middlewares/login.middleware');
+const users = require("./routes/users");
+const sessions = require("./services/sessions.service");
+const jwt = require("./middlewares/login.middleware");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/public', express.static(`${__dirname}/public`));
+app.use("/public", express.static(`${__dirname}/public`));
 app.use(sessions);
 app.use(jwt);
 
-app.get('/', async (req, res) => {
-  res.render('ejs/index.ejs');
+app.get("/", async (req, res) => {
+  res.render("ejs/index.ejs");
 });
-app.use('/users', users);
+app.use("/users", users);
 
-app.listen(3000, () => console.log('server started'));
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+app.listen(3000, () => console.log("server started"));
--- services/sessions.service.js
+++ services/sessions.service.js
@@ -3,7 +3,7 @@ const mysqlStore = require("express-mysql-session")(sessions);
 const { pool } = require("./db.service");
 const db_config = require("../configs/db.config");
 
-const day = ((1000 * 60) ^ 2) * 24;
+const day = 1000 * (60 ^ 3) * 24;
 module.exports = sessions({
   secret: db_config.jwtToken,
   resave: false,
