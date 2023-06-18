65bbd56e1433089955736181f35bb3846c4ae1b8
Nave-Cohen <117146523+Nave-Cohen@users.noreply.github.com>
2023-06-18 19:38:12 +0300

    Nave (#15)
    * labels for modals input
    
    * stop script if schema exist
    
    * addBtn hover grow
    
    * fix sidebar active item
    
    * fix init file

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,67 +1,75 @@
-const $sidebar = $('.sidebar');
+const $sidebar = $(".sidebar");
 const $sidebarNext = $sidebar.next();
-const $searchInput = $('#search-input');
-const $menuIcon = $('#menu-icon');
-const $listItems = $('.list-item:not(.user)');
-const $logoutItem = $('#logout-item');
-
+const $searchInput = $("#search-input");
+const $menuIcon = $("#menu-icon");
+const $listItems = $(".list-item:not(.user)");
+const $logoutItem = $("#logout-item");
 const title = document.title;
+
 getBadges();
-$(`medium:contains(${title})`).parent().addClass('active');
+
+$(".item-text").each(function () {
+  var text = $(this).text().toLowerCase();
+  if (text.includes(title)) {
+    $(this).closest(".list-item").addClass("active");
+  } else {
+    $(this).closest(".list-item").removeClass("active");
+  }
+});
 
 let previousWidth = $sidebar.outerWidth();
-$sidebarNext.css('margin-left', previousWidth + 'px');
+$sidebarNext.css("margin-left", previousWidth + "px");
 
-if (!window.matchMedia('(max-width: 640px)').matches) {
+if (!window.matchMedia("(max-width: 640px)").matches) {
   // On desktop
   setInterval(function () {
     const currentWidth = $sidebar.outerWidth();
     if (currentWidth !== previousWidth) {
-      $sidebarNext.css('margin-left', currentWidth + 'px');
+      $sidebarNext.css("margin-left", currentWidth + "px");
     }
     previousWidth = currentWidth;
   }, 5);
 }
 
-$searchInput.on('focus', function () {
-  $sidebar.addClass('on-search');
+$searchInput.on("focus", function () {
+  $sidebar.addClass("on-search");
 });
 
-$searchInput.on('focusout', function () {
-  $sidebar.removeClass('on-search');
+$searchInput.on("focusout", function () {
+  $sidebar.removeClass("on-search");
 });
 
-$menuIcon.on('click', function () {
-  $sidebar.toggleClass('close');
-  $menuIcon.toggleClass('fa-xmark fa-bars');
+$menuIcon.on("click", function () {
+  $sidebar.toggleClass("close");
+  $menuIcon.toggleClass("fa-xmark fa-bars");
 });
 
-$logoutItem.on('click', function () {
-  window.location.href = '/users/logout';
+$logoutItem.on("click", function () {
+  window.location.href = "/users/logout";
 });
 
-$listItems.on('click', function () {
-  const url = $(this).find('medium').text();
-  window.location.href = '/tasks/' + url;
+$listItems.on("click", function () {
+  const url = $(this).find("medium").text();
+  window.location.href = "/tasks/" + url;
 });
 function getBadges() {
   $.get(
-    '/tasks/updateBadge',
+    "/tasks/updateBadge",
     function (response) {
-      $('#today-badge').text(response.today);
-      $('#upcoming-badge').text(response.upcoming);
+      $("#today-badge").text(response.today);
+      $("#upcoming-badge").text(response.upcoming);
     },
-    'json'
+    "json"
   );
 }
 setInterval(getBadges, 2500);
 
 // eslint-disable-next-line no-unused-vars
 function search(items) {
-  $searchInput.on('input', function () {
+  $searchInput.on("input", function () {
     var inputValue = $(this).val().toLowerCase();
     items.each(function () {
-      if (!$(this).data('title').toLowerCase().includes(inputValue)) {
+      if (!$(this).data("title").toLowerCase().includes(inputValue)) {
         $(this).hide();
       } else {
         $(this).show();
