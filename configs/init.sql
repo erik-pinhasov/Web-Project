07d59f4ec607b1c28dda4931b3323713d4a2e71c
Nave-Cohen <117146523+Nave-Cohen@users.noreply.github.com>
2023-06-25 20:37:37 +0300

    Nave (#28)
    * change project name
    
    * env key name
    
    * syntax
    
    * db to clevercloud
    
    * remove build add tests from action

--- configs/init.sql
+++ configs/init.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `todo_app` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `todo_app`;
+CREATE DATABASE  IF NOT EXISTS `bxetgnt3yjvsykeyetcl` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bxetgnt3yjvsykeyetcl`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: localhost    Database: todo_app
